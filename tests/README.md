945530ca123552a5ff02e54a74e645dcb04b3978
Andrey Sidorov <takandar@gmail.com>
2020-11-15 18:38:53 +0300

    add debounce tests

---
+++ tests/README.md
@@ -0,0 +1,4 @@
+### Debugging
+```sh
+node --inspect ./node_modules/mocha-webpack/lib/cli --webpack-config webpack.config.test.js tests/*.js
+```
--- tests/debounce.spec.js
+++ tests/debounce.spec.js
@@ -5,11 +5,11 @@ import { debounce } from '..';
 
 describe('debounce()', () => {
   it('should debounce when used as a simple decorator', next => {
-    let c = {
+    const c = {
       calls: 0,
       args: null,
 
-      @debounce
+      @debounce(10)
       foo(...args) {
         c.calls++;
         c.args = args;
@@ -22,7 +22,7 @@ describe('debounce()', () => {
     expect(c).to.have.property('calls', 0);
     c.foo(2);
     c.foo(3);
-    setTimeout( () => {
+    setTimeout(() => {
       expect(c).to.have.property('calls', 1);
       expect(c.args).to.deep.equal([3]);
       expect(c.context).to.equal(c);
@@ -31,41 +31,40 @@ describe('debounce()', () => {
     }, 20);
   });
 
-  // it('should debounce when used as a function', next => {
-  //   let c = debounce( (...args) => {
-  //       m.calls++;
-  //       m.args = args;
-  //     }),
-  //     m = { calls:0, args:null };
-  // 
-  //   expect(m).to.have.property('calls', 0);
-  //   c(1);
-  //   expect(m).to.have.property('calls', 0);
-  //   c(2);
-  //   c(3);
-  //   setTimeout( () => {
-  //     expect(m).to.have.property('calls', 1);
-  //     expect(m.args).to.deep.equal([3]);
-  // 
-  //     next();
-  //   }, 20);
-  // });
-  // 
-  // it('should support passing a delay', next => {
-  //   let c = debounce(5, (...args) => {
-  //       m.calls.push(args);
-  //     }),
-  //     m = { calls:[] };
-  // 
-  //   c(1);
-  //   setTimeout(()=> c(2), 1);
-  //   setTimeout(()=> c(3), 10);
-  //   setTimeout(()=> c(4), 14);
-  //   setTimeout(()=> c(5), 22);
-  //   expect(m.calls).to.have.length(0);
-  //   setTimeout( () => {
-  //     expect(m.calls).to.deep.equal([ [2], [4], [5] ]);
-  //     next();
-  //   }, 30);
-  // });
+  it('should debounce when used as a function', next => {
+    const c = debounce(10, (...args) => {
+      m.calls++;
+      m.args = args;
+    });
+    const m = { calls: 0, args: null };
+
+    expect(m).to.have.property('calls', 0);
+    c(1);
+    expect(m).to.have.property('calls', 0);
+    c(2);
+    c(3);
+    setTimeout( () => {
+      expect(m).to.have.property('calls', 1);
+      expect(m.args).to.deep.equal([3]);
+      next();
+    }, 20);
+  });
+
+  it('should support passing a delay', next => {
+    let c = debounce(5, (...args) => {
+        m.calls.push(args);
+      }),
+      m = { calls:[] };
+
+    c(1);
+    setTimeout(()=> c(2), 1);
+    setTimeout(()=> c(3), 10);
+    setTimeout(()=> c(4), 14);
+    setTimeout(()=> c(5), 22);
+    expect(m.calls).to.have.length(0);
+    setTimeout( () => {
+      expect(m.calls).to.deep.equal([ [2], [4], [5] ]);
+      next();
+    }, 30);
+  });
 });
